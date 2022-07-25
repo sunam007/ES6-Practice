6e81112350c7cecc3c9170cd4940719fdf1330d3
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-07-25 11:30:26 +0600

    filter example

--- filter.js
+++ filter.js
@@ -1,3 +1,4 @@
+// filter all the odd number s from an array
 const allNumbers = [2, 4, 6, 8, 9, 11, 13, 15];
 // const callBack = (x) => console.log(x);
 const output = allNumbers.filter((x) => x % 2 == 1);
